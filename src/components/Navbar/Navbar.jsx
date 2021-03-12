2c8904b4bec9ac34267262033f53870aa300750e
Krishnalal P <kaikrishnalal@gmail.com>
2021-03-12 15:51:12 +0530

    Add navigation bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Search } from "../Searchbar/Searchbar";
+import "./Navbar.scss";
+
+export function NavBar() {
+  const history = useHistory();
+  
+  const handleClick = () => {
+    history.push("/");
+  };
+  
+  return (
+    <div className="navbar">
+      <div
+        className="logo"
+        role="button"
+        tabIndex={0}
+        onClick={() => handleClick()}
+        onKeyDown={() => handleClick()}
+      >
+        <img alt="sky_logo" src="sky_logo.png"/>
+      </div>
+      <Search />
+    </div>
+  );
+}
